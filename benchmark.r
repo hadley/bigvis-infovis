8accb6d071c0dbe73ff823d0d6b00bc6b789bde8
hadley <h.wickham@gmail.com>
2013-03-27 13:41:18 -0500

    Rewrite benchmarking section

---
+++ benchmark.r
@@ -0,0 +1,53 @@
+library(bigvis)
+library(ggplot2)
+library(plyr)
+
+n <- 10 ^ c(6, 7, 8)
+n2 <- paste0("x", 6:8)
+names(n) <- n2
+
+bins <- 10 ^ c(2, 3, 4, 5, 6)
+stats <- c("count", "sd", "median")
+
+data <- lapply(n, runif)
+names(data) <- n2
+
+combs <- expand.grid(n2 = n2, bin = bins, stat = stats)
+
+as.data.frame.proc_time <- function(x) {
+  t(x[1:3])
+}
+
+bench <- function(n2, bin, stat) {
+  if (n[[n2]] / bin <= 10) return()
+  xbin <- bin(data[[n2]], 1 / bin)
+  system.time(condense(xbin, summary = stat))
+}
+
+timing <- mdply(combs, bench, .progress = "text")
+saveRDS("timing", "timing.rds")
+
+timing$stat <- factor(timing$stat, levels = c("count", "sd", "median"))
+
+ggplot(timing, aes(bin, elapsed, colour = n2, linetype = stat, shape = stat)) +
+  geom_line(aes(group = interaction(n2, stat)),
+    size = 3, colour = "white", linetype = "solid", show_guide = T) +
+  geom_line(size = 1) +
+  geom_point(size = 3) +
+  scale_x_log10("Number of bins",
+    breaks = 10 ^ c(2, 3, 4, 5, 6), 
+    labels = expression(10 ^ 2, 10 ^ 3, 10 ^ 4, 10 ^ 5, 10 ^ 6)) + 
+  scale_y_log10("Time (s)",
+    breaks = as.vector(c(0.5, 1) %o% 10 ^ c(-2, -1, 0, 1))
+  ) + 
+  scale_colour_brewer("Data size", 
+    palette = "RdPu", 
+    breaks = c("x6", "x7", "x8"),
+    labels = expression(10 ^ 6, 10 ^ 7, 10 ^ 8)) +
+  scale_linetype_manual(values = c("solid", "23", "42")) +
+  guides(
+    colour = guide_legend(reverse = T), 
+    shape = guide_legend("Summary", reverse = T), 
+    linetype = guide_legend("Summary", reverse = T))
+
+ggsave("images/benchmark.pdf", width = 6, height = 4)
